5f844302fbbb4b614e5ffdf389cf1d80b6d0c183
Jesus Estrada <estrada-jesus-@hotmail.com>
2021-01-16 16:03:19 -0600

    Subiendo archivos del proyecto

---
+++ pikachu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pikachu : MonoBehaviour {
+
+    public Transform jugador;
+    public float tamanoRandom;
+    public int puntos;
+    public Text txtPuntos;
+ 
+    void Start()
+    {
+        cambiarPos();
+        txtPuntos.text = puntos.ToString();
+    }
+
+    void sumarPuntos()
+    {
+        puntos++;
+        txtPuntos.text = puntos.ToString();
+    }
+
+    private void OnMouseDown()
+    {
+        cambiarPos();
+        sumarPuntos();
+    }
+     void cambiarPos () 
+    {
+        transform.position = new Vector3((Random.insideUnitSphere.x * tamanoRandom), transform.position.y, (Random.insideUnitSphere.z * tamanoRandom));
+        Vector3 posjugador = new Vector3(jugador.position.x, transform.position.y, jugador.position.z);
+        transform.LookAt (posjugador);
+    }
+
+}
